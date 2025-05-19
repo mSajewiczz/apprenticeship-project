d5a53506e24c84124ec6673f79a277a8bf5c2012
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-19 23:10:24 +0200

    Modified some files

--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace apprenticeship_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string group;
+            RetrieveData retrieveData = new RetrieveData();
+            
+            Console.WriteLine("Write group: ");
+            group = Console.ReadLine();
+            
+            retrieveData.GetData(group);
+        }
+    }
+}
---
+++ apprenticeship-project/RetrieveData.cs
@@ -0,0 +1,19 @@
+﻿namespace apprenticeship_project;
+using System.IO;
+
+public class RetrieveData
+{
+    public string GetData(string group)
+    {
+        String line;
+            StreamReader sr = new StreamReader("C:\\Users\\mikol\\Desktop\\my_code_base\\projects\\apprenticeship-project\\apprenticeship-project\\BASE_123_20250516.csv");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        return group;
+    }
+}
