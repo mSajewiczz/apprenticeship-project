4af22e417f3d1c8c3d5a8cc769f7b72c39d7e43a
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-23 23:35:23 +0200

    Modified some files

--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -5,13 +5,35 @@ namespace apprenticeship_project
     {
         static void Main(string[] args)
         {
-            string group;
+            string[] groups = {"111", "882", "232", "484"};
+            
+            // string group = "";
             RetrieveData retrieveData = new RetrieveData();
-            
-            Console.WriteLine("Write group: ");
-            group = Console.ReadLine();
-            
-            retrieveData.GetData(group);
+
+            Console.WriteLine("Write group (111, 882, 232, 484): ");
+            string group = Console.ReadLine();
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                for (int j = 0; j < groups.Length; j++)
+                {
+                    if (group == groups[j])
+                    {
+                    
+                        retrieveData.GetData(group);
+                        break;
+                    
+                        //nested loop musui tu byc zeby przeleciec tab caly 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Group not found. Try again.");
+                        break;
+                    }
+                }
+            }
         }
+        
+        
     }
 }
--- apprenticeship-project/RetrieveData.cs
+++ apprenticeship-project/RetrieveData.cs
@@ -5,15 +5,39 @@ public class RetrieveData
 {
     public string GetData(string group)
     {
+        int lineCounter = 0;
+        
         String line;
-            StreamReader sr = new StreamReader("C:\\Users\\mikol\\Desktop\\my_code_base\\projects\\apprenticeship-project\\apprenticeship-project\\BASE_123_20250516.csv");
-            line = sr.ReadLine();
+            StreamReader file = new StreamReader("C:\\Users\\mikol\\Desktop\\my_code_base\\projects\\apprenticeship-project\\apprenticeship-project\\BASE_123_20250516.csv");
+            line = file.ReadLine();
             while (line != null)
             {
-                Console.WriteLine(line);
-                line = sr.ReadLine();
+                if (lineCounter != 0)
+                {
+                    // Console.WriteLine(line); //tu cale zdania sa
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        // Console.WriteLine(line[i]);
+
+                        if (line[i] == ';' && line[i+1] == group[0] && line[i+2] == group[1] && line[i+3] == group[2])
+                        {
+                            Console.WriteLine("ok");
+                        }
+                        
+                        
+                        
+                        //tutaj lecisz znakami 
+                    }
+                }
+                
+                line = file.ReadLine();
+                lineCounter++;
+                
             }
-            sr.Close();
+            file.Close();
+            
+            
         return group;
     }
 }
