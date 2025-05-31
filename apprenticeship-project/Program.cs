bd621e4f023e3dbb111a91a4e51de1696aefda9d
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-05-31 23:19:20 +0200

    Idk what im doing

--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -5,47 +5,47 @@ namespace apprenticeship_project
     {
         static void Main(string[] args)
         {
-            string[] groups = {"111", "882", "232", "484"};
             RetrieveData retrieveData = new RetrieveData();
 
             Console.ForegroundColor = ConsoleColor.Green;
-            
-            Console.WriteLine("Get product value (write group 1 - 4): ");
-            
-            Console.ResetColor();
-            string group = Console.ReadLine();
 
-            switch (group)
-            {
-                case "1":
-                    group = "111";
-                    break;
-                case "2":
-                    group = "882";
-                    break;
-                case "3":
-                    group = "232";
-                    break;
-                case "4":
-                    group = "484";
-                    break;
-                default: 
-                    Console.WriteLine("Unknown group");
-                    break;
-            }
+            Console.WriteLine("1 - get quantity of group, 2 - get value of group, 3 - sort group by date (min -> max), 4 - sort group by date (max -> min), 5 - get data of whole file.");
+            Console.Write("Select option: ");
+            string option = Console.ReadLine();
             
-            for (int i = 0; i < groups.Length; i++)
+            bool showMenu = false;
+            string group = "";
+
+            while (!showMenu)
             {
-                for (int j = 0; j < groups.Length; j++)
+                Console.Write("Write group number (1 - 4): ");
+                group = Console.ReadLine();
+            
+                switch (group)
                 {
-                    if (group == groups[j])
-                    {
-                        retrieveData.prepareData(group);
+                    case "1":
+                        showMenu = true;
+                        group = "111";
                         break;
-                    }
+                    case "2":
+                        showMenu = true;
+                        group = "882";
+                        break;
+                    case "3":
+                        showMenu = true;
+                        group = "232";
+                        break;
+                    case "4":
+                        showMenu = true;
+                        group = "484";
+                        break;
+                    default:
+                        Console.WriteLine("Unknown group, try again.");
+                        break;
                 }
-                break;
             }
+            Console.ResetColor();
+            retrieveData.prepareData(group, option);
         }
     }
 }
--- apprenticeship-project/RetrieveData.cs
+++ apprenticeship-project/RetrieveData.cs
@@ -5,98 +5,79 @@ using System.IO;
 
 public class RetrieveData
 {
-    public string prepareData(string group)
+    public void prepareData(string group, string option)
     {
+        // Console.WriteLine("test");
+        //tutaj test tego czy przy wyszukiwaniu kolejnego elementu tez on sie zaczyna od goruy 
         int lineCounter = 0;
-        
+        int counterOfLines = 0;
+
         String line;
-            StreamReader file = new StreamReader("C:\\Users\\mikol\\Desktop\\my_code_base\\projects\\apprenticeship-project\\apprenticeship-project\\BASE_123_20250516.csv");
-            line = file.ReadLine();
+        StreamReader file =
+            new StreamReader(
+                "C:\\Users\\mikol\\Desktop\\my_code_base\\projects\\apprenticeship-project\\apprenticeship-project\\BASE_123_20250516.csv");
+        line = file.ReadLine();
 
-            bool isCompleted = false;
-        
-            
-            while (line != null)
-            {
-                if (lineCounter != 0)
-                {
-                    for (int i = 0; i < line.Length; i++)
-                    {
-                        if (line[i] == ';' && line[i + 1] == group[0] && line[i + 2] == group[1] &&
-                            line[i + 3] == group[2])
-                        {
-                            GetValue(line, group);
-                            break;
-                        }
-                    }
-                }
-                line = file.ReadLine();
-                lineCounter++;
-            }
-            file.Close();
-            
-        return group;
-    }
+        string[] lines = { "", "", "" };
 
-    public void GetLine(String line)
-    {
-       Console.WriteLine(line);
-    }
-
-    public int GetValue(String line, string group)
-    {
-
-        int id = 0;
-        
-        string value = "";
-        int semiColonsCount = 0;
-        
-        for (int i = 0; i < line.Length; i++)
+        while (line != null)
         {
-            if (line[i] == ';')
+            if (lineCounter != 0)
             {
-                semiColonsCount++;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    //ten if ponizej sprawdza czy dana linia ma dana grupe 
+
+                    if (line[i] == ';' && line[i + 1] == group[0] && line[i + 2] == group[1] &&
+                        line[i + 3] == group[2])
+                    {
+                        //tutaj ta metoda ona wyciaga:
+                        //- dana linie pliku,
+                        //- podana przez usera grupe
+
+                        lines[counterOfLines] = line;
+
+                        counterOfLines++;
+                        break;
+                    }
+                }
             }
 
-            if (semiColonsCount == 2)
+            line = file.ReadLine();
+            lineCounter++;
+        }
+
+        file.Close();
+
+        GetQuantity(lines, group);
+    }
+
+    public void GetQuantity(string[] lines, string group)
+    {
+        //grupe + tablice lin gdzie sa informacje wszystkie
+        
+        string[] values = new string[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int semiConlonsCounter = 0;
+            
+            string line = lines[i];
+
+            for (int j = 0; j < line.Length; j++)
             {
-                switch (group)
+                if (line[j] == ';')
                 {
-                    case "111":
-                        group = "1";
-                        break;
-                    case "882":
-                        group = "2";
-                        break;
-                    case "232":
-                        group = "3";
-                        break;
-                    case "484":
-                        group = "4";
-                        break;
+                    semiConlonsCounter++;
                 }
 
-
-                Console.Write("Quantity for group " + group + ": ");
-                
-                for (int j = i+1; j < line.Length; j++)
+                if (semiConlonsCounter == 2)
                 {
-                    if (line[j] == ';')
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.Write(line[j]);
-                    }
+                     
                 }
-                break;
-                
             }
         }
         
-        Console.WriteLine();
-        //zwracac ma tutaj mi te value 
-        return 0;
     }
+
 }
