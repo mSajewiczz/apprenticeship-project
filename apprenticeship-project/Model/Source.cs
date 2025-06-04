d31941a19aab666dba72e9073388c22c90d4dd12
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-06-04 13:24:22 +0200

    Searching files - work in progress

--- apprenticeship-project/Model/Source.cs
+++ apprenticeship-project/Model/Source.cs
@@ -7,18 +7,37 @@ public class Source
     public Source(string fileName)
     {
         //BASE_[3 cyfry]_[data w formacie yyyyMMdd].csv
-        //works: @"(\ABASE_)"
+        //works: @"(\ABASE_)(\d{3})_(\d{8})(\.csv)$";
         
-        string pattern = @"(\ABASE_)(\d{3})_(\d{8})(\.csv)$";
+        const string pattern = @"\ABASE_\d{3}_\d{8}\.csv$";
 
         if (Regex.IsMatch(fileName, pattern))
         {
-            Console.WriteLine("ok");
-            
+            try
+            {
+                Console.WriteLine("ok");
+                var fileExists = File.Exists(fileName);
+                var directoryExists = Directory.Exists(fileName);
+
+                string[] list = Directory.GetFiles(@"c:\", "c*", SearchOption.AllDirectories);
+
+                foreach (string file in list)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Console.WriteLine("File exists: " + fileExists);
+                Console.WriteLine("Directory exists: " + directoryExists);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
         else
         {
-            Console.WriteLine("Unknown file format");
+            Console.WriteLine("Unknown file format. Try again.");
         }
     }
 }
