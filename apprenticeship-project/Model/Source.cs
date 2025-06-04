13feb8d53ed1607db55519bda3989db2421d52ba
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-06-04 12:47:08 +0200

    Creted regex to compare user file name

---
+++ apprenticeship-project/Model/Source.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace apprenticeship_project.Model;
+
+public class Source
+{
+    public Source(string fileName)
+    {
+        //BASE_[3 cyfry]_[data w formacie yyyyMMdd].csv
+        //works: @"(\ABASE_)"
+        
+        string pattern = @"(\ABASE_)(\d{3})_(\d{8})(\.csv)$";
+
+        if (Regex.IsMatch(fileName, pattern))
+        {
+            Console.WriteLine("ok");
+            
+        }
+        else
+        {
+            Console.WriteLine("Unknown file format");
+        }
+    }
+}
--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -1,60 +1,64 @@
 ﻿using System;
+using apprenticeship_project.Model;
+
 namespace apprenticeship_project
 {
     class Program
     {
         static void Main(string[] args)
         {
-            RetrieveData retrieveData = new RetrieveData();
+            // RetrieveData retrieveData = new RetrieveData();
+            //
+            // Console.ForegroundColor = ConsoleColor.Green;
+            //
+            // Console.WriteLine("1 - get quantity of group, 2 - get value of group, 3 - get date min/max of group, 4 - get value of whole file, 0 - exit.");
+            // Console.Write("Select option: ");
+            // string option = Console.ReadLine();
+            // string group = "";
+            //
+            // string type = "null";
+            //
+            // if (option == "0")
+            // {
+            //     Console.WriteLine("Bye!");
+            // }
+            // else
+            // {
+            //     bool showMenu = false;
+            //
+            //     while (!showMenu)
+            //     {
+            //         Console.Write("Write group number (1 - 4): ");
+            //         group = Console.ReadLine();
+            //
+            //         switch (group)
+            //         {
+            //             case "1":
+            //                 showMenu = true;
+            //                 group = "111";
+            //                 break;
+            //             case "2":
+            //                 showMenu = true;
+            //                 group = "882";
+            //                 break;
+            //             case "3":
+            //                 showMenu = true;
+            //                 group = "232";
+            //                 break;
+            //             case "4":
+            //                 showMenu = true;
+            //                 group = "484";
+            //                 break;
+            //             default:
+            //                 Console.WriteLine("Unknown group, try again.");
+            //                 break;
+            //         }
+            //     }
+            //     Console.ResetColor();
+            //     retrieveData.prepareData(group, option);
 
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            Console.WriteLine("1 - get quantity of group, 2 - get value of group, 3 - get date min/max of group, 4 - get value of whole file, 0 - exit.");
-            Console.Write("Select option: ");
-            string option = Console.ReadLine();
-            string group = "";
-
-            string type = "null";
-            
-            if (option == "0")
-            {
-                Console.WriteLine("Bye!");
-            }
-            else
-            {
-                bool showMenu = false;
-
-                while (!showMenu)
-                {
-                    Console.Write("Write group number (1 - 4): ");
-                    group = Console.ReadLine();
-            
-                    switch (group)
-                    {
-                        case "1":
-                            showMenu = true;
-                            group = "111";
-                            break;
-                        case "2":
-                            showMenu = true;
-                            group = "882";
-                            break;
-                        case "3":
-                            showMenu = true;
-                            group = "232";
-                            break;
-                        case "4":
-                            showMenu = true;
-                            group = "484";
-                            break;
-                        default:
-                            Console.WriteLine("Unknown group, try again.");
-                            break;
-                    }
-                }
-                Console.ResetColor();
-                retrieveData.prepareData(group, option);
-            }
+                Source source = new Source("BASE_123_20250516.csv");
+            // }
         }
     }
 }
