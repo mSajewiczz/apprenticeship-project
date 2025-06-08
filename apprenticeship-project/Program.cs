c3ff61391b0bc4ee65d8c1bc7984b62f3be5f8f1
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2025-06-08 17:17:07 +0200

    Start of building RetrieveData class

--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -17,44 +17,13 @@ internal class Program
 
         if (source.CheckFilesStructure)
         {
+            var groupsStrings = source.Groups.ToArray();
+
             while (!test)
             {
                 test = true;
-                Console.WriteLine(
-                    "1 - Get quantity of group, 2 - Get value of group, 3 - Get date min/max of group, 4 - Get value of whole file, 0 - Exit.");
-                Console.Write("Select option: ");
-                option = Console.ReadLine();
-
-                switch (option)
-                {
-                    case "0":
-                        Console.WriteLine("Bye!");
-                        break;
-                    case "1":
-                        option = "1";
-                        break;
-                    case "2":
-                        option = "2";
-                        break;
-                    case "3":
-                        option = "3";
-                        break;
-                    case "4":
-                        option = "4";
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option. Try again.");
-                        test = false;
-                        break;
-                }
-
-                string[] groupsStrings = source.Groups.ToArray();
-
                 Console.Write("Avaliable groups: ");
-                foreach (var g in groupsStrings.Distinct())
-                {
-                    Console.Write(g + ", ");
-                }
+                foreach (var g in groupsStrings.Distinct()) Console.Write(g + ", ");
 
                 Console.Write("\nSelect group: ");
                 group = Console.ReadLine();
@@ -74,7 +43,12 @@ internal class Program
                 }
             }
             
-            RetrieveData retrieveData = new RetrieveData();
+            Console.WriteLine(
+                "1 - Get quantity of group, 2 - Get value of group, 3 - Get date min/max of group, 4 - Get value of whole file, 0 - Exit.");
+            Console.Write("Select option: ");
+            option = Console.ReadLine();
+
+            var retrieveData = new RetrieveData();
             retrieveData.Data(option, group, source.FileContent);
         }
     }
--- apprenticeship-project/RetrieveData.cs
+++ apprenticeship-project/RetrieveData.cs
@@ -1,12 +1,67 @@
 ﻿using System.Collections.Specialized;
 
 namespace apprenticeship_project;
+
 using System.IO;
 
 public class RetrieveData
 {
     public void Data(string option, string group, List<string> FileContent)
     {
-        
+        var checkOption = false;
+
+        while (!checkOption)
+        {
+            checkOption = true;
+
+            switch (option)
+            {
+                case "0":
+                    Console.WriteLine("Bye!");
+                    break;
+                case "1":
+                    GetQuantity(FileContent, group);
+                    break;
+                case "2":
+                    GetValue(FileContent, group);
+                    break;
+                case "3":
+                    GetDate(FileContent, group);
+                    break;
+                case "4":
+                    GetValueOfFile(FileContent);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Try again.");
+                    checkOption = false;
+                    break;
+            }
+            
+            if (!checkOption)
+            {
+                Console.Write("Select option: ");
+                option = Console.ReadLine();
+            }
+        }
+    }
+
+    private void GetQuantity(List<string> FileContent, string group)
+    {
+        Console.WriteLine("1 Works!");
+    }
+
+    private void GetValue(List<string> FileContent, string group)
+    {
+        Console.WriteLine("2 Works!");
+    }
+
+    private void GetDate(List<string> FileContent, string group)
+    {
+        Console.WriteLine("3 Works!");
+    }
+
+    private void GetValueOfFile(List<string> FileContent)
+    {
+        Console.WriteLine("4 Works!");
     }
 }
