f2dd614556a0e1218c21a762120c8a6e27d7632b
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2025-06-13 12:09:22 +0200

    .Split() saved my life (and time)

---
+++ apprenticeship-project/LineModel.cs
@@ -0,0 +1,55 @@
+﻿namespace apprenticeship_project;
+
+public class LineModel
+{
+    private string _group = "";
+    private DateTime _date = new();
+    private double _quantity = 0;
+    private double _value = 0;
+
+    public string Group => _group;
+    public DateTime Date => _date;
+    public double Quantity => _quantity;
+    public double Value => _value;
+
+
+    //LineMethod constructor - have to be invoke to the end of counter that will have to count each line where group = written group by user 
+//CHANGE '.' TO ',' BECAUSE DOUBLE WON'T WORK!!!!!!!!!!!!!!!!!!
+    public LineModel(string line)
+    {
+        var group = "";
+        var date = "";
+        var quantity = "";
+        var value = "";
+        
+        var splitedLine = line.Split(';');
+        var counter = 0;
+        
+        foreach (var item in splitedLine)
+        {
+            if (counter == 2)
+            {
+                quantity = item;
+            }
+            else if (counter == 3)
+            {
+                date = item;
+            }
+            else if (counter == 4)
+            {
+                group = item;
+            }
+            else if (counter == 5)
+            {
+                value = item;
+            }
+
+            counter++;
+        }
+        
+        _group = group;
+        _date = DateTime.Parse(date);
+        _quantity = double.Parse(quantity);
+        _value = double.Parse(value);
+    }
+}
--- apprenticeship-project/Program.cs
+++ apprenticeship-project/Program.cs
@@ -7,47 +7,56 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("--Data Analyse App--");
-        Console.Write("Write path to your file: ");
-        var path = Console.ReadLine();
-        var source = new Source(@path);
-        string option = "", group = "";
-        var groupOk = false;
-        var test = false;
 
-        if (source.CheckFilesStructure)
-        {
-            var groupsStrings = source.Groups.ToArray();
+        LineModel lineModel = new LineModel("1;Produkt 1;10;2025-05-10;111;1,23");
+        
+        Console.WriteLine("Group: " + lineModel.Group);
+        Console.WriteLine("Date: " + lineModel.Date);
+        Console.WriteLine("Value: " + lineModel.Value);
+        Console.WriteLine("Quantity: " + lineModel.Quantity);
+        
 
-            while (!test)
-            {
-                test = true;
-                Console.Write("Avaliable groups: ");
-                foreach (var g in groupsStrings.Distinct()) Console.Write(g + ", ");
-
-                Console.Write("\nSelect group: ");
-                group = Console.ReadLine();
-                Console.WriteLine("-----------------------------------------------------");
-
-                for (var i = 0; i < source.Groups.Count; i++)
-                    if (group == source.Groups[i])
-                        groupOk = true;
-
-                if (!groupOk)
-                {
-                    test = false;
-                    Console.WriteLine("Group not found.");
-                }
-            }
-
-            Console.WriteLine(
-                "1 - Get quantity of group, 2 - Get value of group, 3 - Get date min/max of group, 4 - Get value of whole file, 0 - Exit.");
-            Console.Write("Select option: ");
-            option = Console.ReadLine();
-            Console.WriteLine("-----------------------------------------------------");
-
-            var retrieveData = new RetrieveData();
-            retrieveData.Data(option, group, source.FileContent);
-        }
+        // Console.WriteLine("--Data Analyse App--");
+        // Console.Write("Write path to your file: ");
+        // var path = Console.ReadLine();
+        // var source = new Source(@path);
+        // string option = "", group = "";
+        // var groupOk = false;
+        // var test = false;
+        //
+        // if (source.CheckFilesStructure)
+        // {
+        //     var groupsStrings = source.Groups.ToArray();
+        //
+        //     while (!test)
+        //     {
+        //         test = true;
+        //         Console.Write("Avaliable groups: ");
+        //         foreach (var g in groupsStrings.Distinct()) Console.Write(g + ", ");
+        //
+        //         Console.Write("\nSelect group: ");
+        //         group = Console.ReadLine();
+        //         Console.WriteLine("-----------------------------------------------------");
+        //
+        //         for (var i = 0; i < source.Groups.Count; i++)
+        //             if (group == source.Groups[i])
+        //                 groupOk = true;
+        //
+        //         if (!groupOk)
+        //         {
+        //             test = false;
+        //             Console.WriteLine("Group not found.");
+        //         }
+        //     }
+        //
+        //     Console.WriteLine(
+        //         "1 - Get quantity of group, 2 - Get value of group, 3 - Get date min/max of group, 4 - Get value of whole file, 0 - Exit.");
+        //     Console.Write("Select option: ");
+        //     option = Console.ReadLine();
+        //     Console.WriteLine("-----------------------------------------------------");
+        //
+        //     var retrieveData = new RetrieveData();
+        //     retrieveData.Data(option, group, source.FileContent);
+        // }
     }
 }
