15a0f48ac447ef2baaa28386b084091d04bba4ef
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2025-06-16 09:24:45 +0200

    Added some updates and tested app - works

--- apprenticeship-project/Model/SourceCheck.cs
+++ apprenticeship-project/Model/SourceCheck.cs
@@ -5,23 +5,19 @@ namespace apprenticeship_project.Model;
 public class SourceCheck
 {
     private readonly Regex _pattern = new(@"\ABASE_\d{3}_\d{8}\.csv$");
-    private static int tmp = 20;
+    private static int _tmp = 20;
     private readonly string _path;
-    private char[] _fileNameChars = new char[tmp + 1];
-    private List<char> _groupsChar = new();
+    private char[] _fileNameChars = new char[_tmp + 1];
     private List<string> _fileContent = new();
     private List<string> _groups = new();
     public List<string> FileContent => _fileContent;
     public List<string> Groups => _groups;
-
     private bool CheckFilesStruct()
     {
         var structCorrect = false;
-        var semiColsCount = 0;
-
         for (var k = 0; k < _fileContent.Count; k++)
         {
-            semiColsCount = 0;
+            var semiColsCount = 0;
             var line = _fileContent[k];
             for (var j = 0; j < line.Length; j++)
                 if (line[j] == ';')
@@ -46,47 +42,14 @@ public class SourceCheck
 
         for (var i = 1; i < _fileContent.Count; i++)
         {
-            semiColsCount = 0;
-            var size = 0;
             var line = _fileContent[i];
-
-            char[] charGroup = { };
-
+            var groupStr = ""; 
             for (var j = 0; j < line.Length; j++)
             {
-                if (line[j] == ';') semiColsCount++;
-
-                if (semiColsCount == 4 && line[j] != ';')
-                {
-                    size++;
-                    _groupsChar.Add(line[j + 1]);
-                }
+                var splitedLine = line.Split(';');
+                groupStr = splitedLine[4]; 
             }
-
-            //START YOUR WORK TOMORROW HERE!
-            // for (var j = 0; j < size; j++)
-            // {
-            //     var splitedLine = line.Split(';');
-            //     splitedLine[4] 
-            // }
-
-            charGroup = new char[size];
-            var tmp2 = 0;
-
-            semiColsCount = 0;
-            for (var j = 0; j < line.Length; j++)
-            {
-                if (line[j] == ';') semiColsCount++;
-
-                if (semiColsCount == 4 && line[j] != ';')
-                {
-                    charGroup[tmp2] = line[j];
-                    tmp2++;
-                }
-            }
-
-            var strGroup = new string(charGroup);
-            _groups.Add(strGroup);
+            _groups.Add(groupStr);
         }
         return structCorrect;
     }
@@ -96,15 +59,14 @@ public class SourceCheck
         for (var i = _path.Length; i > 0; i--)
             if (_path[i - 1].Equals('B'))
             {
-                _fileNameChars[tmp] = _path[i - 1];
+                _fileNameChars[_tmp] = _path[i - 1];
                 break;
             }
             else
             {
-                _fileNameChars[tmp] = _path[i - 1];
-                tmp--;
+                _fileNameChars[_tmp] = _path[i - 1];
+                _tmp--;
             }
-
         return new string(_fileNameChars);
     }
     public bool CheckFilesStructure => CheckFilesStruct();
